afe3193d48e5e6c1d79d130b0b64d0ca4d790360
Carlos Isidoro López García <carlosisidoro.lopez@gmail.com>
2017-02-22 19:44:58 +0100

    Initial commit

---
+++ spring-guides-rest/src/main/java/com/calogardev/SpringGuidesRestApplication.java
@@ -0,0 +1,12 @@
+package com.calogardev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGuidesRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGuidesRestApplication.class, args);
+	}
+}
